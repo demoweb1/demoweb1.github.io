b3b1daf51d71c909232fce47401e85e2260b0843
demoweb1 <44314415+demoweb1@users.noreply.github.com>
2018-11-13 08:13:39 +0530

    section color change

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $header_top.find('a').on('click', function() {
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#B8AE9C', '#56bc8a', '#a77dc2', '#36465d', '#fd8d9e'],
+  sectionsColor: ['#B8AE9C', '#56bc8a', '#a77dc2', '#fdd257', '#78ffff'],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
